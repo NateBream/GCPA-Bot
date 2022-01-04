989e22b2978110de500cd3cf2b3ded2eb78900f9
MacGyver Dunham <macgyver.dunham@gmail.com>
2022-01-03 18:58:21 -0700

    added playerinfo portion to create json for new players

--- cogs/addPlayer.py
+++ cogs/addPlayer.py
@@ -1,14 +1,26 @@
+import json
 import shutil
 from player import *
 import openpyxl
 from discord.ext import commands
+
 
 class AddPlayerCog(commands.Cog, name="Adds player"):
     @commands.command()
     async def addPlayer(self, ctx, arg1):
         path = getPlayer(arg1, 'coords')
         path2 = getPlayer(arg1, 'times')
+        path3 = getPlayer(arg1, 'playerinfo')
         player = arg1
+
+        player_info = {
+            "allies": [],
+            "last_alliances": [],
+            "timezone": "",
+            "region_language": "",
+            "coordinates": [],
+            "times": []
+        }
 
         if path == 0:
             wb = open('players/coords/' + player + '.txt', 'w+')
@@ -16,6 +28,11 @@ class AddPlayerCog(commands.Cog, name="Adds player"):
         if path2 == 0:
             wb = open('players/times/' + player + '.txt', 'w+')
             wb.close()
+        if path3 == 0:
+            with open('players/playerinfo/' + player + '.json', 'w') as outfile:
+                json.dump(player_info, outfile)
+                outfile.close()
+
         msg = 'Added player {0}'.format(player)
         await ctx.send('```' + msg + '```')
 
