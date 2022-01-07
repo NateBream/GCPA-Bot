158b5dbe8bb63d7feb7602496f503e3adb525a4e
Nate Bream <nathanielbream@gmail.com>
2022-01-06 21:45:18 -0500

    added ctx argument

--- cogs/shutdown.py
+++ cogs/shutdown.py
@@ -5,7 +5,7 @@ import os
 class ShutdownCog(commands.Cog, name="Shutdown"):
     @commands.command()
     @commands.has_any_role("Admin", "Dev")
-    async def shutdown(self):
+    async def shutdown(self, ctx):
         os.system("./shutdownBot.sh")
         return
 
